2dc0c7887d99d8b08b4b783bbb8992673df31ce7
Ivar <ivabr@stud.ntnu.no>
2023-02-12 19:39:05 +0100

    Added EntityFrameworkCore pakages for working with SQL Server and set up a SQL Server.

---
+++ Simple Authentication Web Application/Model/AuthDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Simple_Authentication_Web_Application.Model
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Simple Authentication Web Application/Program.cs
+++ Simple Authentication Web Application/Program.cs
@@ -1,7 +1,13 @@
+using Simple_Authentication_Web_Application.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthConnectionString")));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AuthDbContext>();
 
 var app = builder.Build();
 
@@ -13,13 +19,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+// Redirects HTTP requests to HTTPS.
 app.UseHttpsRedirection();
+// Enables static files, such as HTML, CSS, images, and JavaScript to be served.
 app.UseStaticFiles();
 
+// Adds route matching to the middleware pipeline.
 app.UseRouting();
 
+// Authorizes a user to access secure resources.
 app.UseAuthorization();
 
+// Configures endpoint routing for Razor Pages.
 app.MapRazorPages();
 
+// Runs the app.
 app.Run();
